8f379b1834b935dea0f137b45d2e7d6d8dec15ee
peter andrew <peter.andrew987@gmail.com>
2023-05-20 17:58:27 +0700

    add swagger docs github pages

--- README.md
+++ README.md
@@ -1,4 +1,18 @@
+# Tech Stack
+
+- Nest Js
+- MongoDB
+- Serverless Framework
+- AWS Lambda
+- AWS Api Gateway
+- AWS S3
+
+# API Documentation
+
+refer to the swagger file
+
 # How to Deploy
 
-1. run `aws configure` and put your IAM creadentials
-2.
+1. copy and fill the `.env` file from `env.sample`
+2. run `aws configure` and put your IAM creadentials
+3. run `npx serverless deploy` to run serverless framework to deploy in aws cloud formation
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import fs from 'fs';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,7 +13,9 @@ async function bootstrap() {
     .setVersion('1.0')
     .build();
   const document = SwaggerModule.createDocument(app, config);
+  fs.writeFileSync('./docs/swagger-spec.json', JSON.stringify(document));
   SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 
